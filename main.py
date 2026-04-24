81dec611a4b1612cf66cbaf3a2a8959775f941f5
rustamovm07-sketch <rustamovm07@icloud.com>
2026-04-24 15:14:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import telebot
+import requests # Boshqa saytlarga buyurtma yuborish uchun
 
-# API Tokeningizni qo'shtirnoq ichiga yozing
-TOKEN = '8659157555:AAG1v3nGfFlc4ZRIq2Pj5N0GqvFYEx6oG_M'
+BOT_TOKEN = '8659157555:AAG1v3nGfFlc4ZRIq2Pj5N0GqvFYEx6oG_M'
+PANEL_API_KEY = '1a321763e449228140a11028135671df' # Mana shu yerga qo'yasiz
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def welcome(message):
-    bot.reply_to(message, "Salom! Men iPhone-dan yaratilgan botman. 24/7 ishlayman!")
+@bot.message_handler(commands=['check'])
+def check_balance(message):
+    # Bu kod panelga borib balansingizni tekshirib keladi
+    url = f"https://turfaseen.net/api/v2?key={PANEL_API_KEY}&action=balance"
+    response = requests.get(url).json()
+    bot.reply_to(message, f"Sizning panelingizdagi balans: {response['balance']} so'm")
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.reply_to(message, f"Siz yozdingiz: {message.text}")
-
-print("Bot ishlashga tayyor...")
 bot.infinity_polling()
+
+
+
